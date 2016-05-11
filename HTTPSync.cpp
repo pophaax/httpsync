2a201955ebd356b65d112290bbb3c3abff34a148
wsoderlu <william.soderlund@ha.ax>
2016-05-11 13:54:45 +0300

    Added pushConfigs call in updateConfigs. Added proper 4 space tabs in most of the functions.

--- HTTPSync.cpp
+++ HTTPSync.cpp
@@ -4,73 +4,70 @@
 
 
 size_t write_to_string(void *ptr, size_t size, size_t count, void *stream) {
- ((std::string*)stream)->append((char*)ptr, 0, size*count);
- return size*count;
+    ((std::string*)stream)->append((char*)ptr, 0, size*count);
+    return size*count;
 }
 
 
 
-HTTPSync::HTTPSync(DBHandler *db,int delay) : m_dbHandler(db)
-{
-  	m_running = true;
-  	curl = curl_easy_init();
-	m_delay = delay;
+HTTPSync::HTTPSync(DBHandler *db,int delay) : m_dbHandler(db) {
+    m_running = true;
+    curl = curl_easy_init();
+    m_delay = delay;
 }
 
 HTTPSync::~HTTPSync() {
- curl_easy_cleanup(curl);
+    curl_easy_cleanup(curl);
 }
 
 
 /* --------------------- taken from sailingrobot ------------------------ */
 
 
-void HTTPSync::run()
-{
- std::cout << "HTTPSync thread started." << std::endl;
- m_logger.info("HTTPSync thread started.");
- setupHTTPSync();
- // updateState();
- // pushConfigs();
- pushWaypoints();
+void HTTPSync::run() {
+    std::cout << "HTTPSync thread started." << std::endl;
+    m_logger.info("HTTPSync thread started.");
+    setupHTTPSync();
+    // updateState();
+    // pushConfigs();
+    pushWaypoints();
 
- while(isRunning())
- {
-   	//updateConfigs();
-   	syncServer();
-	std::this_thread::sleep_for(std::chrono::milliseconds(m_delay));
- }
+    while(isRunning())
+    {
+        //updateConfigs();
+        syncServer();
+        std::this_thread::sleep_for(std::chrono::milliseconds(m_delay));
+    }
 
- std::cout << "HTTPSync thread exited." << std::endl;
- m_logger.info("HTTPSync thread exited.");
+    std::cout << "HTTPSync thread exited." << std::endl;
+    m_logger.info("HTTPSync thread exited.");
 }
 
 void HTTPSync::setupHTTPSync() {
- try {
-   setShipID( m_dbHandler->retrieveCell("server", "1", "boat_id") );
-   setShipPWD( m_dbHandler->retrieveCell("server", "1", "boat_pwd") );
-   setServerURL( m_dbHandler->retrieveCell("server", "1", "srv_addr") );
- } catch (const char * error) {
-   m_logger.error("SailingRobot::setupHTTPSync() failed");
- }
- m_logger.info("setupHTTPSync() done");
+    try {
+        setShipID( m_dbHandler->retrieveCell("server", "1", "boat_id") );
+        setShipPWD( m_dbHandler->retrieveCell("server", "1", "boat_pwd") );
+        setServerURL( m_dbHandler->retrieveCell("server", "1", "srv_addr") );
+    } catch (const char * error) {
+        m_logger.error("SailingRobot::setupHTTPSync() failed");
+    }
+    m_logger.info("setupHTTPSync() done");
 }
 
 void HTTPSync::syncServer() {
- try {
-   // can fetch server response from pushLog
-   pushData(m_dbHandler->getLogs(), "pushAllLogs");
+    try {
+        // can fetch server response from pushLog
+        pushData(m_dbHandler->getLogs(), "pushAllLogs");
 
-   // clearing the datalogs after push.
-   m_dbHandler->clearDatalogTables();
- } catch (const char * error) {
-  //  m_logger.error("Error in HTTPSync::syncserver");
-  //  m_logger.error(std::string(error));
- }
+        // clearing the datalogs after push.
+        m_dbHandler->clearDatalogTables();
+    } catch (const char * error) {
+        //  m_logger.error("Error in HTTPSync::syncserver");
+        //  m_logger.error(std::string(error));
+    }
 }
 
 void HTTPSync::pushWaypoints() {
-
    try {
      pushData(m_dbHandler->getWaypoints(), "pushWaypoints");
    } catch(const char* error) {
@@ -100,13 +97,12 @@ void HTTPSync::setServerURL(std::string URL) {
 }
 
 
-
 std::string HTTPSync::getConfigs(std::string config) {
-  return serve("/?serv=get" + config + "&id=" + shipID + "&pwd=" + shipPWD);
+    return serve("/?serv=get" + config + "&id=" + shipID + "&pwd=" + shipPWD);
 }
 
 std::string HTTPSync::pushData(std::string data, std::string call) {
-  return serve("/?serv=" + call + "&id="+shipID+"&pwd="+shipPWD+"&data="+data);
+    return serve("/?serv=" + call + "&id="+shipID+"&pwd="+shipPWD+"&data="+data);
 }
 
 bool HTTPSync::checkIfNewConfig() {
@@ -122,42 +118,41 @@ void HTTPSync::updateConfigs() {
         std::string configs = getConfigs("AllConfigs");
         m_dbHandler->updateConfigs(configs);
         m_dbHandler->updateTable("state", "configsUpdated", "1","1");
+        pushConfigs();
     }
 }
 
 std::string HTTPSync::serve(std::string serverCall) {
- std::string response = "";
- std::string url = serverURL + serverCall;
- // std::cout << url << std::endl;
- if(curl) {
-   curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-   //curl_easy_setopt(curl, CURLOPT_POST, 1);
-     //curl_easy_setopt(curl, CURLOPT_POSTFIELDS, serverCall.c_str());
-     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_string);
-     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+    std::string response = "";
+    std::string url = serverURL + serverCall;
+    // std::cout << url << std::endl;
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        //curl_easy_setopt(curl, CURLOPT_POST, 1);
+         //curl_easy_setopt(curl, CURLOPT_POSTFIELDS, serverCall.c_str());
+         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_string);
+         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 
-   /* Perform the request, res will get the return code */
-   res = curl_easy_perform(curl);
-   /* Check for errors */
-   if(res != CURLE_OK)
-     throw ( std::string("HTTPSync::serve(): ") + curl_easy_strerror(res) ).c_str();
- }
+        /* Perform the request, res will get the return code */
+        res = curl_easy_perform(curl);
+    /* Check for errors */
+    if(res != CURLE_OK)
+        throw ( std::string("HTTPSync::serve(): ") + curl_easy_strerror(res) ).c_str();
+    }
 
- return response;
+    return response;
 }
 
-bool HTTPSync::isRunning()
-{
- bool running;
- m_mutex.lock();
- running = m_running;
- m_mutex.unlock();
- return running;
+bool HTTPSync::isRunning() {
+    bool running;
+    m_mutex.lock();
+    running = m_running;
+    m_mutex.unlock();
+    return running;
 }
 
-void HTTPSync::close()
-{
- m_mutex.lock();
- m_running = false;
- m_mutex.unlock();
+void HTTPSync::close() {
+    m_mutex.lock();
+    m_running = false;
+    m_mutex.unlock();
 }
